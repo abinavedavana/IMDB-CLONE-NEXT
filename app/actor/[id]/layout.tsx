eac4e939f9b9a33887385b69073cabcabce254ab
abinav964 <abinavedavana@gmail.com>
2026-04-17 18:40:23 +0530

    fix: layout type error

--- app/actor/[id]/layout.tsx
+++ app/actor/[id]/layout.tsx
@@ -1,20 +1,7 @@
-export default function ActorLayout({
-  children,
-  awards,
-  social,
-}: {
-  children: React.ReactNode
-  awards: React.ReactNode
-  social: React.ReactNode
-}) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="space-y-12">
+    <>
       {children}
-
-      {/* <div className="space-y-12 grid grid-cols-2 gap-10">
-        {awards}
-        {social}
-      </div> */}
-    </div>
-  )
+    </>
+  );
 }
