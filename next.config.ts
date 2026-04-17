b4312ab29f8cdc108c9dbdad084694c89e0d7851
abinav964 <abinavedavana@gmail.com>
2026-04-17 19:52:32 +0530

    fix: BroadcastChannel server error

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // FIX FOR BroadcastChannel ERROR
+  serverExternalPackages: [],
+  
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "image.tmdb.org" },
